16905c92e070e9777dc9b0e40564912d2252e871
dragomir1 <16617178+dragomir1@users.noreply.github.com>
2019-08-08 22:43:13 -0700

    setting up components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
-  return <React.Fragment />;
+  return (
+    <React.Fragment>
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </React.Fragment>
+  );
 }
 
 export default App;
